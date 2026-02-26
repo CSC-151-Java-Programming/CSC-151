270f16271b916d2f83fbfd69d508177d93c4a945
Aly3673 <younga3537@student.faytechcc.edu>
2026-02-25 21:25:42 -0500

    Updating files and code.

--- GroupProject_02Prototype.java
+++ GroupProject_02Prototype.java
@@ -1,42 +1,57 @@
+
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.awt.*;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class GroupProject_02Prototype {
-    
     public static void main(String[] args) {
         // Creating file chooser
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new JFrame("Open CSV File");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setSize(400, 200);
+            frame.setLayout(new FlowLayout());
+
+            JButton openButton = new JButton("Open CSV File");
+            openButton.addActionListener(e -> openFile());
+
+            frame.add(openButton);
+            frame.setVisible(true);
+        });
+    }
+        
+        
+    private static void openFile() { 
         JFileChooser fileChooser = new JFileChooser();
-        fileChooser.setDialogTitle("Select a CSV file");
 
-        fileChooser.setFileFilter(new javax.swing.filechooser.FileFilter() {
-            
-        }
+        FileNameExtensionFilter filter = new FileNameExtensionFilter("CSV Files", "csv");
+        fileChooser.setFileFilter(filter);
+        fileChooser.setAcceptAllFileFilterUsed(false);
 
-        // Showing open dialog
+        
         int userSelection = fileChooser.showOpenDialog(null);
-
-        // If the user selects a file
         if (userSelection == JFileChooser.APPROVE_OPTION) {
             File fileToOpen = fileChooser.getSelectedFile();
-            System.out.println("Opening: " + fileToOpen.getAbsolutePath());
-            displayCSV(fileToOpen);
-        } else {
-            System.out.println("No file selected");
+            String filePath = fileToOpen.getAbsolutePath();
+
+            try {
+                readCsvFile(fileToOpen);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
-    private static void displayCSV(File file) {
+    private static void readCsvFile(File file) throws IOException {
         try (BufferedReader br = new BufferedReader(new FileReader(file))) {
             String line;
             while ((line = br.readLine()) != null) {
                 System.out.println(line);
             }
-        } catch (IOException e) {
-            e.printStackTrace();
         }
     }
-
 }
